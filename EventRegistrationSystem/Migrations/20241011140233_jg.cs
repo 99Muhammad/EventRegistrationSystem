72c033d70efccb1410bb7e31d3f4faf13d798ee5
99Muhammad <MuhammadIbraheem99m@gmail.com>
2024-10-11 17:11:45 +0300

    Add project files.

---
+++ EventRegistrationSystem/Migrations/20241011140233_jg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventRegistrationSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class jg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "registrationEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParticipantName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_registrationEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_registrationEvents_events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_registrationEvents_EventId",
+                table: "registrationEvents",
+                column: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "registrationEvents");
+
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
---
+++ EventRegistrationSystem/Program.cs
@@ -0,0 +1,54 @@
+using EventRegistrationSystem.Models.Data;
+using EventRegistrationSystem.Models.Entity;
+using EventRegistrationSystem.Repository.Interface;
+using EventRegistrationSystem.Repository.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventRegistrationSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            string ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+
+            builder.Services.AddDbContext<EventDbContext>(options => options.UseSqlServer(ConnectionString));
+            builder.Services.AddScoped<IEvent, EventService>();
+            builder.Services.AddScoped<IRegistrationEvent, RegistrationEventsService>();
+            builder.Services.AddLogging(config =>
+            {
+                config.AddConsole(); // Logs to the console
+                config.AddDebug();   // Logs to the Debug output window
+                                     // You can add other log providers here
+            });
+            builder.Services.AddScoped<IEmail, EmailService>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
